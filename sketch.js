a7deb52ab0e6ae9395361a325a8889768b7c2faa
Reyansh1920 <71833386+Reyansh1920@users.noreply.github.com>
2021-02-06 17:45:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var form;
+
+
+function setup(){
+    var canvas = createCanvas(displayWidth,displayHeight);
+    form = new Form()
+    form.display()
+    
+}
+
+function draw(){
+    background("lightblue");
+    
+}
